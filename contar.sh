a066bf43767c825b3f6f7c2e3aaea7d4cdbcc45e
Rdurbano <rdurbano@outlook.com>
2023-03-30 16:14:27 -0300

    contar lineas

--- contar.sh
+++ contar.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#!/bin/bash
+
 for i in {1..5}
 do
     
     lineas=$(shuf -i 1-20 -n 1)
-    
+    wc -l loremipsum-$i.txt
     lorem=$(curl -s https://www.lipsum.com/feed/xml\?amount\=$lineas | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g'
 )   
     
     echo "$lorem" >> loremipsum-$i.txt
+    
 done
